88d042427b73f11bcd287ab3eb0314393fda053d
chucoding <chucoding@gmail.com>
2025-10-19 20:13:18 +0900

    refactor : TSK-17 firebase functions 버전 업데이트 및 TS, lint 설정 파일 추가

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,9 @@
-import { initializeApp } from 'firebase-admin/app';
+import {setGlobalOptions} from "firebase-functions";
+import {initializeApp} from "firebase-admin/app";
+
+// For cost control, set the maximum number of containers that can be
+// running at the same time.
+setGlobalOptions({maxInstances: 10});
 
 initializeApp();
 
