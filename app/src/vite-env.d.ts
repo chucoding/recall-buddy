b817b5727a376ba8319c170ce4d714815f269a1c
chucoding <chucoding@gmail.com>
2026-02-17 21:46:50 +0900

    refactor : 불필요한 옵션 제거

--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -5,7 +5,6 @@ interface ImportMetaEnv {
   readonly VITE_MESSAGING_SENDER_ID: string
   readonly VITE_APP_ID: string
   readonly VITE_MEASUREMENT_ID: string
-  readonly VITE_VAPID_KEY: string
   readonly VITE_USER_ID: string
   readonly VITE_SCHEDULE_CODE: string
 }
